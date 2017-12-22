9123e2d28a87eb6dbcac81e29588965505bf13e1
Carlos <carlosmonteroreina@gmail.com>
2017-12-22 12:58:27 +0100

    Actualizado.

--- models/student.js
+++ models/student.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 var student = mongoose.Schema({
     name: String,
     address: String,
-    phone: { home: String, work: String }
+    phone: String
 },{collection:'students'});
 
 module.exports=mongoose.model('Student', student);
